65b5d93034cff5460111f45549191e8fabc2bef3
noskcaj1 <jackson.sbm@gmail.com>
2025-10-24 22:22:11 -0300

    Adicionando controllers / requests - 24.10.2025

---
+++ src/main/java/com/deliverytech/delivery/dto/request/RegisterRequest.java
@@ -0,0 +1,28 @@
+package com.deliverytech.delivery.dto.request;
+
+import com.deliverytech.delivery.model.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRequest {
+
+    @NotBlank(message = "Nome é obrigatório")
+    @Size(min = 2, max = 100, message = "Nome deve ter entre 2 e 100 caracteres")
+    private String nome;
+    
+    @NotBlank(message = "Email é obrigatório")
+    @Email(message = "Email deve ser válido")
+    private String email;
+
+    @NotBlank(message = "Senha é obrigatória")
+    @Size(min = 6, message = "Senha deve ter no mínimo 6 caracteres")
+    private String senha;
+
+    private Role role;
+
+}
